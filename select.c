61962f95dd19df54d7efdf3c5f64abd9ce3e1fd2
Ben <benedict.sh.lo@gmail.com>
2018-02-04 22:59:22 -0800

    working on select

---
+++ select.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include "select.h"
+#include <errno.h>
+#include "main.h"
+#include "macro.h"
+#include "socket.h"
+
+void createSelectFd(fd_set* fd, int newfd, int* maxfd){
+    FD_SET(newfd, fd);
+    if(newfd > *maxfd){
+        *maxfd = newfd;
+    }
+}
+
+void waitForSelectEvent(fd_set * fd, int * maxfd){
+    int n;
+
+    if((n = select(*maxfd+1,fd, NULL, NULL, NULL)) < 0){
+        perror("select error");
+    }
+
+}
+
+size_t singleSelectReadInstance(const int sock, unsigned char *buffer, const size_t bufSize, fd_set* fd, int* maxfd ){
+    FD_ZERO(fd);
+    *maxfd = sock;
+    FD_SET(sock, fd);
+
+    size_t n = 0;
+    int i;
+    waitForSelectEvent(fd, maxfd);
+    int max = *maxfd;
+    for(i = 0; i <= max ; i++){
+        if(FD_ISSET(i, fd)){
+            if(i == sock){
+
+            } else {
+                n = readNBytes(sock, buffer, bufSize);
+            }
+        }
+    }
+    return n;
+}
---
+++ select.h
@@ -0,0 +1,15 @@
+#ifndef select_h
+#define select_h
+
+#include <sys/select.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#define MAX_SELECT 20
+
+void createSelectFd(fd_set* fd, int newfd, int* maxfd);
+//for a blocking call
+size_t singleSelectReadInstance(const int sock, unsigned char *buffer, size_t bufSize, fd_set* fd, int* maxfd);
+void waitForSelectEvent(fd_set* fd, int* maxfd);
+
+#endif
